249a1857c6524150aff29e0721142805c2699c49
dr3dnought <sheremetyev48@mail.ru>
2024-02-11 22:29:26 +0300

    [Feat] Fix `Offer`

---
+++ internal/pkg/models/offer.go
@@ -0,0 +1,61 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Offer struct {
+	ID          uuid.UUID `gorm:"column:id;primaryKey"`
+	AdmitadID   int       `gorm:"column:admitad_id;unique;index"`
+	ShareValue  int       `gorm:"shared_value"`
+	Name        string    `gorm:"name"`
+	Description string    `gorm:"column:description"`
+	ImageURL    string    `gorm:"image_url"`
+
+	CreatedAt *time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt *time.Time `gorm:"column:updated_at;autoUpdateTime"`
+}
+
+func (a *Offer) BeforeCreate(tx *gorm.DB) error {
+	if a.ID == uuid.Nil {
+		id, err := uuid.NewRandom()
+		if err != nil {
+			return err
+		}
+
+		a.ID = id
+	}
+
+	if a.AdmitadID == 0 {
+		return gorm.ErrInvalidValue
+	}
+
+	if err := a.validateShareValue(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *Offer) BeforeUpdate(tx *gorm.DB) error {
+	if a.AdmitadID == 0 {
+		return gorm.ErrInvalidValue
+	}
+
+	if err := a.validateShareValue(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *Offer) validateShareValue() error {
+	if a.ShareValue < 0 || a.ShareValue > 100 {
+		return gorm.ErrInvalidValue
+	}
+
+	return nil
+}
